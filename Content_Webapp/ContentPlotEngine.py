ff186203232786e5af76d586953d41752bf04e68
ShoumikMajumdar <shoumikmajumdar@gmail.com>
2021-01-04 15:21:06 -0500

    Added Genre based

---
+++ Content_Webapp/ContentPlotEngine.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  3 14:46:11 2021
+
+@author: Shoum
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+from ast import literal_eval
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+from sklearn.feature_extraction.text import CountVectorizer
+import pickle
+
+
+class Reco_content:
+    def __init__(self):
+        self.data = pd.read_csv("movies_metadata.csv")
+        self.data = self.data.drop([19730,29503,35587])
+        self.data['genres'] = self.data['genres'].fillna('[]').apply(literal_eval).apply(lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
+        
+        
+        self.credits = pd.read_csv("credits.csv")
+        self.keywords = pd.read_csv("keywords.csv")
+
+        self.keywords['id'] = self.keywords['id'].astype('int')
+        self.credits['id'] = self.credits['id'].astype('int')
+        self.data['id'] = self.data['id'].astype('int')
+
+        self.df = self.data[['adult','genres','title','overview','vote_average','vote_count']]
+        self.df = self.df.iloc[:20000,:]
+        self.df['overview'] = self.df['overview'].fillna('')
+
+        # Plot
+
+        self.tfidf = TfidfVectorizer(stop_words='english')
+        self.tfidf_matrix = self.tfidf.fit_transform(self.df['overview'])
+        self.cosine_sim = linear_kernel(self.tfidf_matrix,self.tfidf_matrix)
+
+        # Genre
+
+        self.merged = self.data.merge(self.keywords,on='id')
+        self.merged = self.merged.merge(self.credits,on='id')
+            
+        features = ['cast', 'crew', 'keywords']
+        for feature in features:
+            self.merged[feature] = self.merged[feature].apply(literal_eval)
+    
+        features = ['cast', 'keywords']
+        for feature in features:
+            self.merged[feature] = self.merged[feature].apply(self.get_list)
+
+        self.merged['director'] = self.merged['crew'].apply(self.get_director)
+        self.merged['Screenplay'] = self.merged['crew'].apply(self.get_screenplay)
+        self.merged = self.merged[['genres','title','keywords','cast','director','Screenplay']]
+
+        features = ['genres','keywords','cast','director','Screenplay']
+        for feature in features:
+            self.merged[feature] = self.merged[feature].apply(self.clean_data)
+
+        self.merged['metadata'] = self.merged.apply(self.soup,axis = 1)
+
+        self.merged = self.merged.iloc[:20000,:]
+
+        self.count = CountVectorizer(stop_words='english')
+        self.count_matrix = self.count.fit_transform(self.merged['metadata'])
+        self.cosine_sim = linear_kernel(self.count_matrix,self.count_matrix)
+                       
+    def cold_start(self, title,percentile=0.95):
+        vote_avg = self.df[self.df['vote_average'].notnull()]['vote_average'].astype('int')
+        C = vote_avg.mean()
+        
+        vote_ct = self.df[self.df['vote_count'].notnull()]['vote_count'].astype('int')
+        m = vote_ct.quantile(percentile)
+        
+        qualified = self.df[(self.df['vote_count'] >= m) & (self.df['vote_average'].notnull()) & (self.df['vote_count'].notnull())][['title','vote_count', 'vote_average']]
+        
+        qualified['wr'] = qualified.apply(lambda x: (x['vote_count']/(x['vote_count']+m) * x['vote_average']) + (m/(m+x['vote_count']) * C), axis=1)
+        qualified = qualified.sort_values('wr', ascending=False).head(10)
+        
+        qualified = qualified.values.tolist()
+        return qualified
+
+    #Get crew list
+    def get_list(self,x):
+        if isinstance(x,list):
+            words = []
+            for i in x:
+                words.append(i['name'])
+            
+            if len(words)>3:
+                words = words[:3]
+                
+            return words
+        return[]
+
+    #get director list
+    def get_director(self,x):
+        for i in x:
+            if i['job'] == 'Director':
+                return i['name']
+        return np.nan
+
+    #get screenplay list
+    def get_screenplay(self,x):
+        names = []
+        for i in x:
+            if i['job'] == 'Screenplay':
+                return i['name']
+        
+        return np.nan
+
+    #clean data to create soup
+    def clean_data(self,x):
+        if isinstance(x, list):
+            return [str.lower(i.replace(" ", "")) for i in x]
+        else:
+            #Check if director exists. If not, return empty string
+            if isinstance(x, str):
+                return str.lower(x.replace(" ", ""))
+            else:
+                return ''
+
+    def soup(self,x):
+        return ' '.join(x['keywords']) + ' ' + ' '.join(x['cast']) + ' ' + x['director'] + ' ' + ' '.join(x['Screenplay'])+ ' ' + ' '.join(x['genres'])
+    
+
+    """
+    Get recommendations based on keywords in the plot
+    """
+    def get_reco_plot(self,title):
+        indices = pd.Series(self.df.index, index = self.df['title'])
+        try:
+            idx = indices[title]
+        except KeyError:
+            res = self.cold_start(title)
+            return res
+        sim_score = list(enumerate(self.cosine_sim[idx]))
+        sim_score = sorted(sim_score,key = lambda x : x[1], reverse = True)
+        sim_score = sim_score[1:11]
+        movie_indices = [i[0] for i in sim_score]
+        res =  self.df['title'].iloc[movie_indices]
+        res = res.values.tolist()
+        return res
+
+    """
+    Get recommendations based on genre, cast, director and screenwriter
+    """
+
+    def get_reco_genre_credits(self,title):
+        indices = pd.Series(self.merged.index , index = self.merged['title'])
+        try:
+            ind = indices[title]
+        except KeyError:
+            res = self.cold_start(title)
+            return res
+        sim_scores = list(enumerate(self.cosine_sim[ind]))
+        sim_scores = sorted(sim_scores, key = lambda x: x[1], reverse=True)
+        sim_scores = sim_scores[1:11]
+        movie_indices = [i[0] for i in  sim_scores]
+        res = self.merged['title'].iloc[movie_indices]
+        res = res.values.tolist()
+        return res
+
+
+
+   
--- Content_Webapp/app.py
+++ Content_Webapp/app.py
@@ -1,10 +1,11 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
-from Engine import Reco
+from ContentPlotEngine import Reco_plot
+#from ContentPlotEngine import get_director get_list, get_reco, get_reco_genre_credits,get_reco_plot,get_screenplay,clean_data
 
 app = Flask(__name__)
-model = Reco()
+model = Reco_content()
 
 @app.route('/')
 def home():
@@ -14,7 +15,7 @@ def home():
 @app.route('/predict',methods=[ 'POST','GET'])
 def predict():
     title = request.form['movie']
-    res = model.get_reco(title)
+    res = model.get_reco_genre_credits(title)
     return jsonify(res)
     
 
